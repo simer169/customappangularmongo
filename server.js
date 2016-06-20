e629f1c4bf303af66008e9738a30fa59524e78e1
Simer <simer@gmail.com>
2016-06-20 14:31:48 +0530

    modules added

--- server.js
+++ server.js
@@ -15,12 +15,8 @@ mongoose.connect('mongodb://pankaj:test1234@ds015934.mlab.com:15934/todo', funct
 	}
 });
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride());
+// app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+
 
 // listen (start app with node server.js) ======================================
 app.listen();
